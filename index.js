9b594b259c18e9a3356421294d3addf87f9d7650
Arch881010 <111789310+Arch881010@users.noreply.github.com>
2022-10-21 09:54:09 -0500

    Self Refreshing for testing purpose

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
+//waittime settings every 1000 = 1 second.
+waittime = 1000;
+//Buffer
 function panic() {
     window.close();
 }
 function refresh() {
     document.reload();
 }
+// TIMEOUT/DOCUMENT.reload for sanity
+setTimeout(() => {
+    document.refresh();
+  }, waittime)
