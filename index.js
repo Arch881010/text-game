edc7df13e013ded0baf5b9847429ba3d05bb9ce8
Arch881010 <111789310+Arch881010@users.noreply.github.com>
2022-10-24 11:25:15 -0500

    JS additions

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 //waittime settings every 1000 = 1 second.
-waittime = 50000;
+//60000 = 1 minute.
+waittime = 60000;
 //Buffer
 function panic() {
     close();
 }
 // TIMEOUT/DOCUMENT.reload for sanity
-
+const submit = document.getElementById('submit');
+submit.onclick = function (){
+var location = document.getElementById("locationselecter").value;
+}
 setTimeout(() => {
     window.location.reload();
   }, waittime)
