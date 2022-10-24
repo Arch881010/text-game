0aca18518ff5bb379bd48c8c461b7e669a802758
Arch881010 <111789310+Arch881010@users.noreply.github.com>
2022-10-24 11:32:02 -0500

    JS chanegs

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+//Sets location, etc. 
+var numlocation = 0;
+function num() {
+    if (Location === "01") {
+        numlocation = 1;
+    } else if (Location === "02") {
+        numlocation = 2;
+    } else {
+        console.log("Location is not valid.");
+        alert("Check Console");
+    }
+}
 //waittime settings every 1000 = 1 second.
 //60000 = 1 minute.
 waittime = 60000;
@@ -9,6 +21,7 @@ function panic() {
 const submit = document.getElementById('submit');
 submit.onclick = function (){
 var Location = document.getElementById("locationselecter").value;
+num();
 }
 // TIMEOUT/DOCUMENT.reload for sanity
 setTimeout(() => {
